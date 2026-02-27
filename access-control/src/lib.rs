f683b4c6c5e176ecac3342eb59fa735a3c638079
Ndifreke000 <ndifrekemkpanam@gmail.com>
2026-02-27 17:11:59 +0100

    chore: apply cargo fmt and remove unnecessary files

--- access-control/src/lib.rs
+++ access-control/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, Address, Env, Symbol, Vec, symbol_short};
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Address, Env, Symbol, Vec};
 
 #[derive(Clone)]
 #[contracttype]
